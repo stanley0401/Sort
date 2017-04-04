a61ff3263d167b71a0f01a796863ffe73153c799
keyan <keyan@192.168.1.125>
2017-04-04 00:21:47 -0700

    sort method

--- src/SelectionSorter.java
+++ src/SelectionSorter.java
@@ -11,7 +11,19 @@ public class SelectionSorter
     public static void sort(int[] a)
     {  
         // to do.  add you selection sort here
-
+    	for(int i = 0; i < a.length; i++)
+    		ArrayUtil.swap(a, i, getSmallestIndex(a, i));
+    }
+    
+    public static int getSmallestIndex(int[] a, int startingIndex)
+    {
+    	int smallestIndex = startingIndex;
+    	for(int i = startingIndex + 1; i<a.length; i++)
+    	{
+    		if(a[smallestIndex]>a[i])
+    			smallestIndex = i;
+    	}
+    	return smallestIndex;
     }
 }
 
